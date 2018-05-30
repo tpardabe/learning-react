858a470bfc522c74eaf07239c30d49561fcc666c
Talgat Pardabekov <555tauga@mail.ru>
2018-05-30 12:11:31 +0600

    Add files via upload
    added a work experience page

--- About.jsx
+++ About.jsx
@@ -4,19 +4,17 @@ class About extends React.Component {
 	render() {
 		//todo: describe myself
 		const linkedInLink = 'https:\/\/www.linkedin.com';
-		const githubLink = 'https:\/\/github.com'
+		const githubLink = 'https:\/\/github.com\/tpardabe';
 		return (
 			<div class="outer">
 				<div class="middle">
 					<div class="inner">
-						<div class="container">
-							<h2>About</h2>
+						<h2>About</h2>
 							<h4>Back end Software Engineer</h4>
 							<ul class="list-inline">
 								<li><a href={linkedInLink} class="fa fa-linkedin-square" id="link"/></li>
 								<li><a href={githubLink} class="fa fa-git-square" id="link"/></li>
 							</ul>
-						</div>
 					</div>
 				</div>
 			</div>
--- Experience.jsx
+++ Experience.jsx
@@ -3,7 +3,40 @@ import React from 'react';
 class Experience extends React.Component {
 	render() {
 		//todo: describe myself
-		return <div>My Experience</div>;
+		return (
+				<div class="timeline">
+				  <div class="container left">
+				    <div class="content">
+				      <h2><font color="white">2017</font></h2>
+				      <p>
+				      	<font color="white">
+				      		Alfa Bank
+				      	</font>
+				      </p>
+				    </div>
+				  </div>
+				  <div class="container right">
+				    <div class="content">
+				       <h2><font color="white">2016</font></h2>
+				      <p>
+				      	<font color="white">
+				      		Dar Ecosystem
+				      	</font>
+		      		  </p>
+				    </div>
+				  </div>
+				  <div class="container left">
+				    <div class="content">
+				      <h2><font color="white">2015</font></h2>
+				      <p>
+				      	<font color="white">
+				      		Halva Loyalty
+				      	</font>
+				      </p>
+				    </div>
+				  </div>
+				</div>
+			);
 	}
 }
 
